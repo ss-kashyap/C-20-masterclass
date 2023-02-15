9501b226033fa1acbcab988444f416b9f4119f64
S.S.Kashyap <shashangkak@gmail.com>
2023-02-15 17:59:31 +0530

    commited

---
+++ 15.CharacterManipulationAndStrings/one defination rule/main.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+#include "Person.h"
+//variable : Declaration and Defdination
+/*
+double weight = 0;
+
+double add(double a, double b){
+    return a+b;
+}
+
+struct point{
+    double m_x {};
+    double m_y {};
+};
+*/
+
+int main(){
+    
+Person p1("sskashyap", 19);
+
+p1.print_info();
+return 0;
+
+    
+}
